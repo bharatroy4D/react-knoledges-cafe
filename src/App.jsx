8f1a77cc1f742829009006b51af507044a740151
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-15 12:51:22 +0600

    react knoledge cafe start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl bg-green-300 text-center py-3'>React Knoledge cafe</h1>
+    
+    </>
+  )
+}
+
+export default App
