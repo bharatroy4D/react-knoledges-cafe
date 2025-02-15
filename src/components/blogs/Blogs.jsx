ed2c0d63478c80761ba7b0a62c3325cd516fbe36
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-15 13:52:12 +0600

    data fetch by useEffect

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Blogs = () => {
+    const [blog, setBlog] = useState([]);
+    console.log(blog)
+    useEffect(() =>{
+        fetch('blogs.json')
+        .then(res =>res.json())
+        .then(data =>setBlog(data))
+    },[])
     return (
         <div>
-            
         </div>
     );
 };
