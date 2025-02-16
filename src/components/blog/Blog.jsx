2d0acf0a990d9ec2e6b9559697c4e819491ed91a
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-16 16:48:46 +0600

    project into author, title, author_img added

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,11 +1,28 @@
-
-const Blog = ({blog}) => {
-    const {id, title, cover, author, author_img, hashtags } = blog; 
+import PropTypes from "prop-types";
+const Blog = ({ blog }) => {
+    const {title, cover, author, author_img, hashtags, reading_time } = blog;
     return (
         <div>
-            
+            <img src={cover} alt="" />
+            <div className="flex items-center justify-between">
+                <div className="flex gap-3 py-2 items-center">
+                    <img className="w-12 h-12" src={author_img} alt="" />
+                    <div>
+                        <h3 className="font-bold text-lg">{author}</h3>
+                        <h3 className="">{title}</h3>
+                    </div>
+                </div>
+                <div>
+                <h2>{reading_time} <span>min Read</span></h2>
+                </div>
+            </div>
         </div>
+
     );
 };
 
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+
+}
 export default Blog;
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -5,18 +5,18 @@ import Blog from "../blog/Blog";
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
     console.log(blogs)
-    useEffect(() =>{
+    console.log(blogs)
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res =>res.json())
-        .then(data =>setBlogs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
         <div className=" w-2/3 ">
-            <h2 className="text-3xl">blogs</h2>
             {
-                blogs.map(blog =><Blog
-                key={blog.id}
-                blog ={blog}
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
                 ></Blog>)
             }
         </div>
